ce35f2fbc570463d3966e68f3e894e4057b92872
Scott Chen <rightson@gmail.com>
2016-08-31 21:47:07 +0800

    clang-format: WebKit style

--- examples/LoRa/SX_01a_TX_LoRa.cpp
+++ examples/LoRa/SX_01a_TX_LoRa.cpp
@@ -27,70 +27,71 @@
 
 int e;
 
-char message1 [] = "Packet 1, wanting to see if received packet is the same as sent packet";
-char message2 [] = "Packet 2, broadcast test";
+char message1[] = "Packet 1, wanting to see if received packet is the same as sent packet";
+char message2[] = "Packet 2, broadcast test";
 
 void setup()
 {
-  // Print a start message
-  printf("SX1272 module and Raspberry Pi: send packets without ACK\n");
-  
-  // Power ON the module
-  e = sx1272.ON();
-  printf("Setting power ON: state %d\n", e);
-  
-  // Set transmission mode
-  e |= sx1272.setMode(4);
-  printf("Setting Mode: state %d\n", e);
-  
-  // Set header
-  e |= sx1272.setHeaderON();
-  printf("Setting Header ON: state %d\n", e);
-  
-  // Select frequency channel
-  e |= sx1272.setChannel(CH_10_868);
-  printf("Setting Channel: state %d\n", e);
-  
-  // Set CRC
-  e |= sx1272.setCRC_ON();
-  printf("Setting CRC ON: state %d\n", e);
-  
-  // Select output power (Max, High or Low)
-  e |= sx1272.setPower('H');
-  printf("Setting Power: state %d\n", e);
-  
-  // Set the node address
-  e |= sx1272.setNodeAddress(3);
-  printf("Setting Node address: state %d\n", e);
-  
-  // Print a success message
-  if (e == 0)
-    printf("SX1272 successfully configured\n");
-  else
-    printf("SX1272 initialization failed\n");
+    // Print a start message
+    printf("SX1272 module and Raspberry Pi: send packets without ACK\n");
 
-  delay(1000);
+    // Power ON the module
+    e = sx1272.ON();
+    printf("Setting power ON: state %d\n", e);
+
+    // Set transmission mode
+    e |= sx1272.setMode(4);
+    printf("Setting Mode: state %d\n", e);
+
+    // Set header
+    e |= sx1272.setHeaderON();
+    printf("Setting Header ON: state %d\n", e);
+
+    // Select frequency channel
+    e |= sx1272.setChannel(CH_10_868);
+    printf("Setting Channel: state %d\n", e);
+
+    // Set CRC
+    e |= sx1272.setCRC_ON();
+    printf("Setting CRC ON: state %d\n", e);
+
+    // Select output power (Max, High or Low)
+    e |= sx1272.setPower('H');
+    printf("Setting Power: state %d\n", e);
+
+    // Set the node address
+    e |= sx1272.setNodeAddress(3);
+    printf("Setting Node address: state %d\n", e);
+
+    // Print a success message
+    if (e == 0)
+        printf("SX1272 successfully configured\n");
+    else
+        printf("SX1272 initialization failed\n");
+
+    delay(1000);
 }
 
 void loop(void)
 {
-	// Send message1 and print the result
+    // Send message1 and print the result
     e = sx1272.sendPacketTimeout(8, message1);
-    printf("Packet sent, state %d\n",e);
-    
+    printf("Packet sent, state %d\n", e);
+
     delay(4000);
- 
- 	// Send message2 broadcast and print the result
+
+    // Send message2 broadcast and print the result
     e = sx1272.sendPacketTimeout(0, message2);
-    printf("Packet sent, state %d\n",e);
-    
+    printf("Packet sent, state %d\n", e);
+
     delay(4000);
 }
 
-int main (){
-	setup();
-	while(1){
-		loop();
-	}
-	return (0);
+int main()
+{
+    setup();
+    while (1) {
+        loop();
+    }
+    return (0);
 }
--- examples/LoRa/SX_01b_RX_LoRa.cpp
+++ examples/LoRa/SX_01b_RX_LoRa.cpp
@@ -21,8 +21,8 @@
  *  Design:            David Gascón 
  *  Implementation:    Covadonga Albiñana, Victor Boria, Ruben Martin
  */
- 
-// Include the SX1272 and SPI library: 
+
+// Include the SX1272 and SPI library:
 #include "arduPiLoRa.h"
 
 int e;
@@ -30,69 +30,67 @@ char my_packet[100];
 
 void setup()
 {
-  // Print a start message
-  printf("SX1272 module and Raspberry Pi: receive packets without ACK\n");
-  
-  // Power ON the module
-  e = sx1272.ON();
-  printf("Setting power ON: state %d\n", e);
-  
-  // Set transmission mode
-  e |= sx1272.setMode(4);
-  printf("Setting Mode: state %d\n", e);
-  
-  // Set header
-  e |= sx1272.setHeaderON();
-  printf("Setting Header ON: state %d\n", e);
-  
-  // Select frequency channel
-  e |= sx1272.setChannel(CH_10_868);
-  printf("Setting Channel: state %d\n", e);
-  
-  // Set CRC
-  e |= sx1272.setCRC_ON();
-  printf("Setting CRC ON: state %d\n", e);
-  
-  // Select output power (Max, High or Low)
-  e |= sx1272.setPower('H');
-  printf("Setting Power: state %d\n", e);
-  
-  // Set the node address
-  e |= sx1272.setNodeAddress(8);
-  printf("Setting Node address: state %d\n", e);
-  
-  // Print a success message
-  if (e == 0)
-    printf("SX1272 successfully configured\n");
-  else
-    printf("SX1272 initialization failed\n");
+    // Print a start message
+    printf("SX1272 module and Raspberry Pi: receive packets without ACK\n");
 
-  delay(1000);
+    // Power ON the module
+    e = sx1272.ON();
+    printf("Setting power ON: state %d\n", e);
+
+    // Set transmission mode
+    e |= sx1272.setMode(4);
+    printf("Setting Mode: state %d\n", e);
+
+    // Set header
+    e |= sx1272.setHeaderON();
+    printf("Setting Header ON: state %d\n", e);
+
+    // Select frequency channel
+    e |= sx1272.setChannel(CH_10_868);
+    printf("Setting Channel: state %d\n", e);
+
+    // Set CRC
+    e |= sx1272.setCRC_ON();
+    printf("Setting CRC ON: state %d\n", e);
+
+    // Select output power (Max, High or Low)
+    e |= sx1272.setPower('H');
+    printf("Setting Power: state %d\n", e);
+
+    // Set the node address
+    e |= sx1272.setNodeAddress(8);
+    printf("Setting Node address: state %d\n", e);
+
+    // Print a success message
+    if (e == 0)
+        printf("SX1272 successfully configured\n");
+    else
+        printf("SX1272 initialization failed\n");
+
+    delay(1000);
 }
 
 void loop(void)
 {
-  // Receive message
-  e = sx1272.receivePacketTimeout(10000);
-  if ( e == 0 )
-  {
-    printf("Receive packet, state %d\n",e);
+    // Receive message
+    e = sx1272.receivePacketTimeout(10000);
+    if (e == 0) {
+        printf("Receive packet, state %d\n", e);
 
-    for (unsigned int i = 0; i < sx1272.packet_received.length; i++)
-    {
-      my_packet[i] = (char)sx1272.packet_received.data[i];
+        for (unsigned int i = 0; i < sx1272.packet_received.length; i++) {
+            my_packet[i] = (char)sx1272.packet_received.data[i];
+        }
+        printf("Message: %s\n", my_packet);
+    } else {
+        printf("Receive packet, state %d\n", e);
     }
-    printf("Message: %s\n", my_packet);
-  }
-  else {
-    printf("Receive packet, state %d\n",e);
-  }
 }
 
-int main (){
-	setup();
-	while(1){
-		loop();
-	}
-	return (0);
+int main()
+{
+    setup();
+    while (1) {
+        loop();
+    }
+    return (0);
 }
